d99c9745b4b656ef334d6ab42fab4fc929c30dc8
Justin Munn <design@justinwmunn.com>
2015-11-26 02:09:37 +0000

    Added plan model and database table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126001123) do
+ActiveRecord::Schema.define(version: 20151126020051) do
+
+  create_table "plans", force: true do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
